88698830585c3bc840750d68d88c345f676b25e1
Jasur-Rustamov <isakovamatlyuba1959@gmail.com>
2024-11-14 13:46:06 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/**@type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html, css}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': "#079211",
+        'paragrph': '#808080',
+        'box-bg': '#313131',
+        'deep-bray': '#0A0A0F',
+        'deep-dark': '#050507',
+      },
+    },
+  },
+  plugins: [],
+};
